a2025194063c14eafa881dccb2de35354941fe50
dieaster <102417732+dieaster@users.noreply.github.com>
2024-02-14 22:55:59 +0300

    Create calc.js

---
+++ calc.js
@@ -0,0 +1,9 @@
+// input
+const numero1 = parseInt(prompt('enter the first number '));
+const numero2 = parseInt(prompt('enter the second number '));
+
+//add two numbers
+const sum = numero1 + numero2;
+
+// display
+console.log(`The sum of ${numero1} and ${numero2} is ${sum}`);
